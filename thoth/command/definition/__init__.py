cdd42094880d89a55a8e10794d1d213da2f8ca7e
eric <eric.nieuwland@gmail.com>
2025-10-17 16:29:22 +0200

    WIP

--- thoth/command/definition/__init__.py
+++ thoth/command/definition/__init__.py
@@ -4,16 +4,8 @@
 import typer
 
 # own imports
-
+from .new_definition import new_definition
 
 app = typer.Typer()
 
-app.command("check")(check_norm)
-app.command("languages")(languages_in_norm)
-app.command("new")(new_norm)
-app.command("reformat")(reformat_norm)
-app.command("render")(render_norm)
-app.command("render-translated")(render_translated_norm)
-app.command("split")(split_norm)
-app.command("update")(update_norm)
-app.command("xcheck")(xcheck_norm)
+app.command("new")(new_definition)
