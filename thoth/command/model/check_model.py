44379fb65d651df620f6b0bc539024ebcca8d409
eric <eric.nieuwland@gmail.com>
2025-10-30 07:42:21 +0100

    WIP

--- thoth/command/model/check_model.py
+++ thoth/command/model/check_model.py
@@ -5,10 +5,8 @@ check_model - check syntax of a document model
 from __future__ import annotations
 
 # standard library imports
-from pathlib import Path
 
 # third party imports
-import typer
 
 # own imports
 from thoth.command.shared.arguments_and_options_info import (
