a889f48e332ac15721a8b68eca14d2ccc515c651
eric <eric.nieuwland@gmail.com>
2025-11-06 15:49:18 +0100

    WIP

--- tests/test__command/test__command__shared/test__print_changes.py
+++ tests/test__command/test__command__shared/test__print_changes.py
@@ -1,9 +1,128 @@
 import unittest
 from unittest.mock import patch
 
-from io import StringIO
+from thoth.command.shared.print_changes import human_centric_diff, print_changes
 
-from thoth.command.shared.print_changes import print_changes
+
+class TestHumanCentricDiff(unittest.TestCase):
+    """
+    test human_centric_diff() function
+    """
+
+    def setUp(self) -> None:
+        self.maxDiff = None
+
+    def test_empties(self) -> None:
+        # given
+        for original in ([], ""):
+            for changed in ([], ""):
+                # when
+                actual = list(human_centric_diff(original, changed))
+                # then
+                expect = []
+                self.assertListEqual(expect, actual, msg=f"failed for {original!r} and {changed!r}")
+
+    def test_one_line_added(self) -> None:
+        # given
+        original = ""
+        changed = "foo bar"
+        # when
+        actual = list(human_centric_diff(original, changed))
+        # then
+        expect = [
+            "+     foo bar",
+        ]
+        self.assertListEqual(expect, actual)
+
+    def test_one_line_removed(self) -> None:
+        # given
+        original = "foo bar"
+        changed = ""
+        # when
+        actual = list(human_centric_diff(original, changed))
+        # then
+        expect = [
+            "-   1 foo bar",
+        ]
+        self.assertListEqual(expect, actual)
+
+    def test_one_line_changed(self) -> None:
+        # given
+        original = "foo bar"
+        changed = "baz qux"
+        # when
+        actual = list(human_centric_diff(original, changed))
+        # then
+        expect = [
+            "-   1 foo bar",
+            "+     baz qux",
+        ]
+        self.assertListEqual(expect, actual)
+
+    def test_interior_removed(self) -> None:
+        # given
+        original = """
+foo
+bar
+baz
+        """.strip()
+        changed = [
+            "foo",
+            "baz",
+        ]
+        # when
+        actual = list(human_centric_diff(original, changed))
+        # then
+        expect = [
+            "    1 foo",
+            "-   2 bar",
+            "    3 baz",
+        ]
+        self.assertListEqual(expect, actual)
+
+    def test_interior_added(self) -> None:
+        # given
+        original = [
+            "foo",
+            "baz",
+        ]
+        changed = [
+            "foo",
+            "bar",
+            "baz",
+        ]
+        # when
+        actual = list(human_centric_diff(original, changed))
+        # then
+        expect = [
+            "    1 foo",
+            "+     bar",
+            "    2 baz",
+        ]
+        self.assertListEqual(expect, actual)
+
+    def test_interior_changed(self) -> None:
+        # given
+        original = [
+            "foo",
+            "bar",
+            "baz",
+        ]
+        changed = [
+            "foo",
+            "qux",
+            "baz",
+        ]
+        # when
+        actual = list(human_centric_diff(original, changed))
+        # then
+        expect = [
+            "    1 foo",
+            "-   2 bar",
+            "+     qux",
+            "    3 baz",
+        ]
+        self.assertListEqual(expect, actual)
 
 
 class TestPrintChanges(unittest.TestCase):
@@ -14,138 +133,25 @@ class TestPrintChanges(unittest.TestCase):
     def setUp(self) -> None:
         self.maxDiff = None
 
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_empties(self, mock_stdout) -> None:
+    @patch("thoth.command.shared.print_changes.human_centric_diff")
+    def test_no_change(self, mock_human_centric_diff) -> None:
         # given
-        for original in ([], ""):
-            for changed in ([], ""):
-                # when
-                actual = print_changes(original, changed)
-                # then
-                expect = False
-                self.assertEqual(expect, actual, msg=f"failed for {original!r} and {changed!r}")
-                expect = ""
-                self.assertEqual(expect, mock_stdout.getvalue(), msg=f"failed for {original!r} and {changed!r}")
-
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_one_line_added(self, mock_stdout) -> None:
-        # given
-        original = ""
-        changed = "foo bar"
+        mock_human_centric_diff.return_value = []
         # when
-        actual = print_changes(original, changed)
+        actual = print_changes("", "")
+        # then
+        expect = False
+        self.assertEqual(expect, actual)
+
+    @patch("thoth.command.shared.print_changes.human_centric_diff")
+    def test_change(self, mock_human_centric_diff) -> None:
+        # given
+        mock_human_centric_diff.return_value = [""]
+        # when
+        actual = print_changes("", "")
         # then
         expect = True
         self.assertEqual(expect, actual)
-        expect = """
-+     foo bar
-        """.strip()
-        self.assertEqual(expect, mock_stdout.getvalue().strip())
-
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_one_line_removed(self, mock_stdout) -> None:
-        # given
-        original = "foo bar"
-        changed = ""
-        # when
-        actual = print_changes(original, changed)
-        # then
-        expect = True
-        self.assertEqual(expect, actual)
-        expect = """
--   1 foo bar
-        """.strip()
-        self.assertEqual(expect, mock_stdout.getvalue().strip())
-
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_one_line_changed(self, mock_stdout) -> None:
-        # given
-        original = "foo bar"
-        changed = "baz qux"
-        # when
-        actual = print_changes(original, changed)
-        # then
-        expect = True
-        self.assertEqual(expect, actual)
-        expect = """
--   1 foo bar
-+     baz qux
-        """.strip()
-        self.assertEqual(expect, mock_stdout.getvalue().strip())
-
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_interior_removed(self, mock_stdout) -> None:
-        # given
-        original = """
-foo
-bar
-baz
-        """.strip()
-        changed = """
-foo
-baz
-        """.strip()
-        # when
-        actual = print_changes(original, changed)
-        # then
-        expect = True
-        self.assertEqual(expect, actual)
-        expect = """
-    1 foo
--   2 bar
-    3 baz
-        """.strip()
-        self.assertEqual(expect, mock_stdout.getvalue().strip())
-
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_interior_added(self, mock_stdout) -> None:
-        # given
-        original = """
-foo
-baz
-        """.strip()
-        changed = """
-foo
-bar
-baz
-        """.strip()
-        # when
-        actual = print_changes(original, changed)
-        # then
-        expect = True
-        self.assertEqual(expect, actual)
-        expect = """
-    1 foo
-+     bar
-    2 baz
-        """.strip()
-        self.assertEqual(expect, mock_stdout.getvalue().strip())
-
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_interior_changed(self, mock_stdout) -> None:
-        # given
-        original = """
-foo
-bar
-baz
-        """.strip()
-        changed = """
-foo
-qux
-baz
-        """.strip()
-        # when
-        actual = print_changes(original, changed)
-        # then
-        expect = True
-        self.assertEqual(expect, actual)
-        expect = """
-    1 foo
--   2 bar
-+     qux
-    3 baz
-        """.strip()
-        self.assertEqual(expect, mock_stdout.getvalue().strip())
 
 
 if __name__ == "__main__":
--- thoth/command/shared/print_changes.py
+++ thoth/command/shared/print_changes.py
@@ -7,32 +7,31 @@ from __future__ import annotations
 # standard library imports
 import re
 from difflib import unified_diff
+from collections.abc import Iterable
 
 # third party imports
 
 # own imports
 
 
-def print_changes(original: str | list[str], changed: str | list[str]) -> bool:
+def human_centric_diff(original: str | list[str], changed: str | list[str]) -> Iterable[str]:
     """
     compare an original and changed version of a text
-    If they differ, present the changes in a user-friendly way.
-    Return True if changes were found, False if they were not.
+    present the changes in a user-friendly way
     """
     if isinstance(original, str):
         original = original.splitlines()
     if isinstance(changed, str):
         changed = changed.splitlines()
-    changes = False
+
     line_nr = 0  # keep IDE and QA tools happy
     for line in unified_diff(original, changed, lineterm=""):
-        changes = True
         if line in ("--- ", "+++ "):
             continue
         if found := re.match(r"@@ -(?P<first>\d+).* @@", line):
             line_nr = int(found["first"])
             if line_nr > 1:
-                print("  ...")
+                yield "  ..."
             continue
         first = line[0]
         line = line[1:]
@@ -41,5 +40,19 @@ def print_changes(original: str | list[str], changed: str | list[str]) -> bool:
         else:
             lnr = f"{line_nr:3d}"
             line_nr += 1
-        print(f"{first} {lnr} {line}")
+        yield f"{first} {lnr} {line}"
+
+
+def print_changes(original: str | list[str], changed: str | list[str]) -> bool:
+    """
+    compare an original and changed version of a text
+    If they differ, print the changes in a user-friendly way.
+    Return True if changes were found, False if they were not.
+    """
+    changes = False
+
+    for line in human_centric_diff(original, changed):
+        changes = True
+        print(line)
+
     return changes
